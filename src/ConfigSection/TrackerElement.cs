93a9f337e20c69d7ba226ff5a54843c2d9328ad9
Sarin Na Wangkanai <sarin@wangkanai.com>
2020-02-08 11:02:38 +0700

    Update to .NET Standard

--- src/ConfigSection/TrackerElement.cs
+++ src/ConfigSection/TrackerElement.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Configuration;
-
-namespace Wangkanai.Universal.ConfigSection
+﻿namespace Wangkanai.Universal.ConfigSection
 {
     class TrackerElement : ConfigurationElement
     {
